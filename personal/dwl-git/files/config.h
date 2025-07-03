558f0c0043c7637b5c975188dde74345b52b9bda
balls <sunnyred123@protonmail.com>
2025-07-03 17:30:43 +0000

    update

--- personal/dwl-git/files/config.h
+++ personal/dwl-git/files/config.h
@@ -114,7 +114,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "foot", NULL };
-static const char *menucmd[] = { "wmenu-run", "-f12", "-N000000", "-n999999", "-S000000", NULL };
+static const char *menucmd[] = { "mew-run", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
